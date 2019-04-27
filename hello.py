eeea204ad2b029388d45fdc288d975d599ad766b
mendhak <mendhak@users.noreply.github.com>
2019-04-27 10:59:07 +0100

    Simplified image display

--- hello.py
+++ hello.py
@@ -12,16 +12,25 @@ try:
     print("Clear")
     epd.Clear(0xFF)
     
-     
-    print("read bmp file on window")
+    print("read image file")
     start=time.time()
-    Himage2 = Image.new('1', (epd7in5.EPD_WIDTH, epd7in5.EPD_HEIGHT), 255)  # 255: clear the frame
-    bmp = Image.open('wiki.jpg')
-    Himage2.paste(bmp, (0,0))
-    epd.display(epd.getbuffer(Himage2))
+    image = Image.new('1', (epd7in5.EPD_WIDTH, epd7in5.EPD_HEIGHT), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(image)
+    image = Image.open('cat.png')
+    epd.display(epd.getbuffer(image))
     end=time.time()
     print("That took " + str(end - start))
     time.sleep(2)
+     
+    # print("read bmp file on window")
+    # start=time.time()
+    # Himage2 = Image.new('1', (epd7in5.EPD_WIDTH, epd7in5.EPD_HEIGHT), 255)  # 255: clear the frame
+    # bmp = Image.open('cat.png')
+    # Himage2.paste(bmp, (0,0))
+    # epd.display(epd.getbuffer(Himage2))
+    # end=time.time()
+    # print("That took " + str(end - start))
+    # time.sleep(2)
         
     epd.sleep()
         

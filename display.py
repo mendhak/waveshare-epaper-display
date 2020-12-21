af4982cce23c5ea7388fa3f1df75bb5fe05fe31c
mendhak <mendhak@users.noreply.github.com>
2020-12-21 15:35:03 +0000

    Create display.py

---
+++ display.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+import sys
+import os
+#picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+#libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+libdir = "/home/pi/e-Paper/RaspberryPi_JetsonNano/python/lib"
+if os.path.exists(libdir):
+    print("Found")
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in5 as epd7in5
+import time, datetime
+from PIL import Image
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd7in5 Demo")
+
+    epd = epd7in5.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    if datetime.datetime.now().minute==0:
+        epd.Clear()
+
+    logging.info("3.read bmp file")
+    #Himage = Image.open("/home/pi/waveshare-epaper-display/screen-output.bmp")
+    Himage = Image.open(sys.argv[1])
+    epd.display(epd.getbuffer(Himage))
+    epd.sleep()
+    epd.Dev_exit()
+
+except IOError as e:
+    logging.info(e)
+
+except KeyboardInterrupt:
+    logging.info("ctrl + c:")
+    epd7in5.epdconfig.module_exit()
+    exit()

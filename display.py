ed7c168c28d1fc4a6bb69058ffad300e6a3e29e1
mendhak <mendhak@users.noreply.github.com>
2021-01-31 20:32:41 +0000

    Specify import and height/width based on EPD version

--- display.py
+++ display.py
@@ -11,9 +11,11 @@ if os.path.exists(libdir):
 
 import logging
 
-##If you have a Waveshare v2 screen, try changing this to 
-#from waveshare_epd import epd7in5_V2 as epd7in5
-from waveshare_epd import epd7in5 as epd7in5
+if os.getenv("WAVESHARE_EPD75_VERSION",1):
+    from waveshare_epd import epd7in5 as epd7in5
+else:
+    from waveshare_epd import epd7in5_V2 as epd7in5
+
 
 import time, datetime
 from PIL import Image
--- run.sh
+++ run.sh
@@ -14,6 +14,15 @@ sudo -E python3 screen-calendar-get.py
 
 figlet Export
 # Inkscape can't export to BMP, so let's export to PNG first. 
+
+if [ $WAVESHARE_EPD75_VERSION = 1 ]; then 
+    WAVESHARE_WIDTH=640
+    WAVESHARE_HEIGHT=384
+else
+    WAVESHARE_WIDTH=800
+    WAVESHARE_HEIGHT=480
+fi
+
 inkscape  screen-output-weather.svg --without-gui -e screen-output.png -w$WAVESHARE_WIDTH -h$WAVESHARE_HEIGHT --export-dpi=150
 
 # Convert to a black and white, 1 bit bitmap

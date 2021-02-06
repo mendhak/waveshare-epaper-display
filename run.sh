ac913925ea40d00ab1b7115a8d8d20b931f2b15d
mendhak <mendhak@users.noreply.github.com>
2021-02-06 14:11:31 +0000

    Rendering PNG to the screen, since it takes same amount of time as BMP

--- run.sh
+++ run.sh
@@ -10,9 +10,9 @@ figlet Calendar info
 sudo -E python3 screen-calendar-get.py
 
 figlet Export
-# Inkscape can't export to BMP, so let's export to PNG first. 
+# Inkscape can't export to BMP, so let's export to PNG first.
 
-if [ $WAVESHARE_EPD75_VERSION = 1 ]; then 
+if [ $WAVESHARE_EPD75_VERSION = 1 ]; then
     WAVESHARE_WIDTH=640
     WAVESHARE_HEIGHT=384
 else
@@ -20,14 +20,14 @@ else
     WAVESHARE_HEIGHT=480
 fi
 
-inkscape  screen-output-weather.svg --without-gui -e screen-output.png -w$WAVESHARE_WIDTH -h$WAVESHARE_HEIGHT --export-dpi=150
+inkscape screen-output-weather.svg --without-gui -e screen-output.png -w$WAVESHARE_WIDTH -h$WAVESHARE_HEIGHT --export-dpi=300
 
 # Convert to a black and white, 1 bit bitmap
-convert -colors 2 +dither -type Bilevel -monochrome screen-output.png screen-output.bmp
+#convert -colors 2 +dither -type Bilevel -monochrome screen-output.png screen-output.bmp
 
 
 SHOULD_REFRESH=0
 current_minute=`date +"%M"`
 
 figlet Display
-sudo python3 display.py screen-output.bmp 
+sudo python3 display.py screen-output.png

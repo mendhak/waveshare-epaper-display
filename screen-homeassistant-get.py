3d62a7a4ae3180bf2a9253cdbc840876a01e1ab7
Justin Mason <Justin.Mason@swrve.com>
2021-03-26 21:52:44 +0000

    Add a lightning emoji beside the car range if the cars are charging

--- screen-homeassistant-get.py
+++ screen-homeassistant-get.py
@@ -18,16 +18,31 @@ auth_headers = {
 }
 
 output = codecs.open(template , 'r', encoding='utf-8').read()
+charge_icon = "\N{high voltage sign}"
 
 for sensor in sensors:
   r = requests.get(hass_url + 'api/states/' + sensor,
 	  headers=auth_headers)
-  ## print(r.text)
+  print(r.text)
 
   j = json.loads(r.text)
-  val = int(j["state"])
-  output = output.replace(sensors[sensor], str(val))
-  print(sensor + ": " + str(val))
+
+  if "binary_sensor" in sensor:
+    val = j["state"]
+    if "on" in val:
+      val = charge_icon
+    else:
+      val = ""
+
+  else:
+    val = str(int(j["state"]))
+    # support this weird API for MercedesME: https://community.home-assistant.io/t/mercedes-me-component/41911/590
+    if "chargingactive" in j and "chargingstatus" in j:
+      if int(j["chargingstatus"]) in [0,1]:
+        val = val + charge_icon
+
+  output = output.replace(sensors[sensor], val)
+  print(sensor + ": " + val)
 
 codecs.open('screen-output-homeassistant.svg', 'w', encoding='utf-8').write(output)
 

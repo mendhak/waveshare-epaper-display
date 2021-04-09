83120e5b090b3a4081ecfde34deed2ecaeaaeb57
Justin Mason <Justin.Mason@swrve.com>
2021-04-09 10:05:41 +0100

    Refactor to cleanly support 'unavailable' error responses from HomeAssistant

--- screen-homeassistant-get.py
+++ screen-homeassistant-get.py
@@ -1,4 +1,4 @@
-# Scrape sensor values from a local HomeAssistant instance.
+# sCRAPE SENSOR VALUes from a local HomeAssistant instance.
 
 from __future__ import print_function
 import codecs
@@ -17,32 +17,50 @@ auth_headers = {
     'Content-Type': 'application/json'
 }
 
-output = codecs.open(template , 'r', encoding='utf-8').read()
 charge_icon = "\N{high voltage sign}"
 
-for sensor in sensors:
-  r = requests.get(hass_url + 'api/states/' + sensor,
-	  headers=auth_headers)
-  print(r.text)
+def scrape_sensors(output):
+  for sensor in sensors:
+    r = requests.get(hass_url + 'api/states/' + sensor,
+	    headers=auth_headers)
+    print(r.text)
+  
+    j = json.loads(r.text)
+    if "binary_sensor" in sensor:
+      val = parse_binary_sensor(j)
+    else:
+      val = parse_sensor(j)
+  
+    output = output.replace(sensors[sensor], val)
+    print(sensor + ": " + val)
+  return output
 
-  j = json.loads(r.text)
-
-  if "binary_sensor" in sensor:
+def parse_binary_sensor(j):
+  try:
     val = j["state"]
     if "on" in val:
-      val = charge_icon
+      return charge_icon
     else:
-      val = ""
+      return ""
+  except:   # state not found; could be the sensor is unavailable
+    return ""
 
-  else:
+def parse_sensor(j):
+  try:
     val = str(int(j["state"]))
     # support this weird API for MercedesME: https://community.home-assistant.io/t/mercedes-me-component/41911/590
-    if "chargingactive" in j and "chargingstatus" in j:
-      if int(j["chargingstatus"]) in [0,1]:
-        val = val + charge_icon
+    if "attributes" in j:
+      attrs = j["attributes"]
+      if "chargingactive" in attrs and "chargingstatus" in attrs:
+        if int(attrs["chargingstatus"]) in [0,1]:
+          val = val + charge_icon
+    return val
 
-  output = output.replace(sensors[sensor], val)
-  print(sensor + ": " + val)
+  except:   # casting to int failed, often because the value is the str literal "unavailable"
+    return ""
 
+
+output = codecs.open(template , 'r', encoding='utf-8').read()
+output = scrape_sensors(output)
 codecs.open('screen-output-homeassistant.svg', 'w', encoding='utf-8').write(output)
 

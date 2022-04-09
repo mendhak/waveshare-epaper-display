da32b5f05d5749f1af2b5bf3b94506cf7e4beea7
mendhak <mendhak@users.noreply.github.com>
2022-04-09 17:11:55 +0100

    Wind icon

--- screen-weather-get.py
+++ screen-weather-get.py
@@ -153,7 +153,7 @@ def main():
     output_dict = {
         'LOW_ONE': "{}{}".format(str(round(weather['temperatureMin'])), degrees),
         'HIGH_ONE': "{}{}".format(str(round(weather['temperatureMax'])), degrees),
-        'ICON_ONE': 'thundershower_rain',
+        'ICON_ONE': 'wind',
         'WEATHER_DESC_1': weather_desc[1],
         'WEATHER_DESC_2': weather_desc[2],
         'TIME_NOW': datetime.datetime.now().strftime("%-I:%M %p"),

7ecde3782f0735d3739aeb2ca21ecb9301bdcfee
mendhak <mendhak@users.noreply.github.com>
2022-04-10 21:30:05 +0100

    Add some helpful comments to the custom template files

--- screen-custom-get.py
+++ screen-custom-get.py
@@ -6,6 +6,8 @@ configure_logging()
 def main():
     output_svg_filename = 'screen-custom.svg'
 
+    # If you make changes to this file be sure to make a backup in case you ever update! 
+    
     # Add custom code here like getting PiHole Status, car charger status, API calls. 
     # Assign the value you want to display to custom_value_1, and it will replace CUSTOM_DATA_1 in screen-custom.svg.
     # You can edit the screen-custom.svg to change appearance, position, font size, add more custom data. 

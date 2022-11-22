15d795023ec9a95a43c8bbd5a0e9e286335480b7
mendhak <mendhak@users.noreply.github.com>
2022-11-22 23:04:03 +0000

    Move google calendar into provider module

---
+++ calendar_providers/google.py
@@ -0,0 +1,97 @@
+import datetime
+from calendar_providers.base_provider import BaseCalendarProvider, CalendarEvent
+from utility import is_stale
+import os
+import logging
+import pickle
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+
+ttl = float(os.getenv("CALENDAR_TTL", 1 * 60 * 60))
+
+
+class GoogleCalendar(BaseCalendarProvider):
+    def __init__(self, google_calendar_id, max_event_results, from_date, to_date):
+        self.max_event_results = max_event_results
+        self.from_date = from_date
+        self.to_date = to_date
+        self.google_calendar_id = google_calendar_id
+
+    def get_google_credentials(self):
+
+        google_token_pickle = 'token.pickle'
+        google_credentials_json = 'credentials.json'
+        google_api_scopes = ['https://www.googleapis.com/auth/calendar.readonly']
+
+        credentials = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists(google_token_pickle):
+            with open(google_token_pickle, 'rb') as token:
+                credentials = pickle.load(token)
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not credentials or not credentials.valid:
+            if credentials and credentials.expired and credentials.refresh_token:
+                credentials.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    google_credentials_json, google_api_scopes)
+                credentials = flow.run_local_server()
+            # Save the credentials for the next run
+            with open(google_token_pickle, 'wb') as token:
+                pickle.dump(credentials, token)
+
+        return credentials
+
+    def get_calendar_events(self) -> list[CalendarEvent]:
+        calendar_events = []
+        google_calendar_pickle = 'cache_calendar.pickle'
+
+        service = build('calendar', 'v3', credentials=self.get_google_credentials(), cache_discovery=False)
+
+        events_result = None
+
+        if is_stale(os.getcwd() + "/" + google_calendar_pickle, ttl):
+            logging.debug("Pickle is stale, calling the Calendar API")
+
+            # Call the Calendar API
+            events_result = service.events().list(
+                calendarId=self.google_calendar_id,
+                timeMin=self.from_date.isoformat() + 'Z',
+                maxResults=self.max_event_results,
+                singleEvents=True,
+                orderBy='startTime').execute()
+
+            for event in events_result.get('items', []):
+                if event['start'].get('date'):
+                    is_all_day = True
+                    start_date = datetime.datetime.strptime(event['start'].get('date'), "%Y-%m-%d")
+                    end_date = datetime.datetime.strptime(event['end'].get('date'), "%Y-%m-%d")
+                    # Google Calendar marks the 'end' of all-day-events as
+                    # the day _after_ the last day. eg, Today's all day event ends tomorrow!
+                    # So subtract a day
+                    end_date = end_date - datetime.timedelta(days=1)
+                else:
+                    is_all_day = False
+                    start_date = datetime.datetime.strptime(event['start'].get('dateTime'), "%Y-%m-%dT%H:%M:%S%z")
+                    end_date = datetime.datetime.strptime(event['end'].get('dateTime'), "%Y-%m-%dT%H:%M:%S%z")
+
+                summary = event['summary']
+
+                calendar_events.append(CalendarEvent(summary, start_date, end_date, is_all_day))
+
+            with open(google_calendar_pickle, 'wb') as cal:
+                pickle.dump(calendar_events, cal)
+
+        else:
+            logging.info("Found in cache")
+            with open(google_calendar_pickle, 'rb') as cal:
+                calendar_events = pickle.load(cal)
+
+        if len(calendar_events) == 0:
+            logging.info("No upcoming events found.")
+
+        return calendar_events
--- screen-calendar-get.py
+++ screen-calendar-get.py
@@ -1,16 +1,13 @@
 import datetime
-import pickle
 import os.path
 import os
 import logging
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from googleapiclient.discovery import build
 from calendar_providers.base_provider import CalendarEvent
 from calendar_providers.caldav import CalDavCalendar
+from calendar_providers.google import GoogleCalendar
 from calendar_providers.ics import ICSCalendar
 from calendar_providers.outlook import OutlookCalendar
-from utility import is_stale, update_svg, configure_logging, get_formatted_date
+from utility import update_svg, configure_logging, get_formatted_date
 
 
 configure_logging()
@@ -29,115 +26,6 @@ caldav_calendar_id = os.getenv("CALDAV_CALENDAR_ID", None)
 ics_calendar_url = os.getenv("ICS_CALENDAR_URL", None)
 
 ttl = float(os.getenv("CALENDAR_TTL", 1 * 60 * 60))
-
-
-def get_google_credentials():
-
-    google_token_pickle = 'token.pickle'
-    google_credentials_json = 'credentials.json'
-    google_api_scopes = ['https://www.googleapis.com/auth/calendar.readonly']
-
-    credentials = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(google_token_pickle):
-        with open(google_token_pickle, 'rb') as token:
-            credentials = pickle.load(token)
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not credentials or not credentials.valid:
-        if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                google_credentials_json, google_api_scopes)
-            credentials = flow.run_local_server()
-        # Save the credentials for the next run
-        with open(google_token_pickle, 'wb') as token:
-            pickle.dump(credentials, token)
-
-    return credentials
-
-
-def get_google_events(max_event_results):
-
-    google_calendar_pickle = 'cache_calendar.pickle'
-
-    service = build('calendar', 'v3', credentials=get_google_credentials(), cache_discovery=False)
-
-    events_result = None
-
-    if is_stale(os.getcwd() + "/" + google_calendar_pickle, ttl):
-        logging.debug("Pickle is stale, calling the Calendar API")
-
-        today_start_time = datetime.datetime.utcnow()
-        if os.getenv("CALENDAR_INCLUDE_PAST_EVENTS_FOR_TODAY", "0") == "1":
-            today_start_time = datetime.datetime.combine(datetime.datetime.utcnow(), datetime.datetime.min.time())
-        # Call the Calendar API
-        events_result = service.events().list(
-            calendarId=google_calendar_id,
-            timeMin=today_start_time.isoformat() + 'Z',
-            maxResults=max_event_results,
-            singleEvents=True,
-            orderBy='startTime').execute()
-
-        with open(google_calendar_pickle, 'wb') as cal:
-            pickle.dump(events_result, cal)
-
-    else:
-        logging.info("Found in cache")
-        with open(google_calendar_pickle, 'rb') as cal:
-            events_result = pickle.load(cal)
-
-    events = events_result.get('items', [])
-
-    if not events:
-        logging.info("No upcoming events found.")
-
-    return events
-
-
-def get_output_dict_from_google_events(events, event_slot_count):
-    formatted_events = {}
-    event_count = len(events)
-    for event_i in range(event_slot_count):
-        event_label_id = str(event_i + 1)
-        if (event_i <= event_count - 1):
-            formatted_events['CAL_DATETIME_' + event_label_id] = \
-                get_google_datetime_formatted(events[event_i]['start'], events[event_i]['end'])
-            formatted_events['CAL_DESC_' + event_label_id] = events[event_i]['summary']
-        else:
-            formatted_events['CAL_DATETIME_' + event_label_id] = ""
-            formatted_events['CAL_DESC_' + event_label_id] = ""
-    return formatted_events
-
-
-def get_google_datetime_formatted(event_start, event_end):
-    if event_start.get('dateTime'):
-        start_date = datetime.datetime.strptime(event_start.get('dateTime'), "%Y-%m-%dT%H:%M:%S%z")
-        end_date = datetime.datetime.strptime(event_end.get('dateTime'), "%Y-%m-%dT%H:%M:%S%z")
-        if start_date.date() == end_date.date():
-            start_formatted = get_formatted_date(start_date)
-            end_formatted = end_date.strftime("%-I:%M %p")
-        else:
-            start_formatted = get_formatted_date(start_date)
-            end_formatted = get_formatted_date(end_date)
-        day = "{} - {}".format(start_formatted, end_formatted)
-    else:
-        start = datetime.datetime.strptime(event_start.get('date'), "%Y-%m-%d")
-        end = datetime.datetime.strptime(event_end.get('date'), "%Y-%m-%d")
-        # Google Calendar marks the 'end' of all-day-events as
-        # the day _after_ the last day. eg, Today's all day event ends tomorrow!
-        # So subtract a day
-        end = end - datetime.timedelta(days=1)
-        start_day = get_formatted_date(start, include_time=False)
-        end_day = get_formatted_date(end, include_time=False)
-        if start == end:
-            day = start_day
-        else:
-            day = "{} - {}".format(start_day, end_day)
-    return day
 
 
 def get_formatted_calendar_events(fetched_events: list[CalendarEvent]) -> dict:
@@ -195,23 +83,19 @@ def main():
     if outlook_calendar_id:
         logging.info("Fetching Outlook Calendar Events")
         provider = OutlookCalendar(outlook_calendar_id, max_event_results, today_start_time, oneyearlater_iso)
-        calendar_events = provider.get_calendar_events()
-        output_dict = get_formatted_calendar_events(calendar_events)
     elif caldav_calendar_url:
         logging.info("Fetching Caldav Calendar Events")
         provider = CalDavCalendar(caldav_calendar_url, caldav_calendar_id, max_event_results,
                                   today_start_time, oneyearlater_iso, caldav_username, caldav_password)
-        calendar_events = provider.get_calendar_events()
-        output_dict = get_formatted_calendar_events(calendar_events)
     elif ics_calendar_url:
         logging.info("Fetching ics Calendar Events")
         provider = ICSCalendar(ics_calendar_url, max_event_results, today_start_time, oneyearlater_iso)
-        calendar_events = provider.get_calendar_events()
-        output_dict = get_formatted_calendar_events(calendar_events)
     else:
         logging.info("Fetching Google Calendar Events")
-        google_events = get_google_events(max_event_results)
-        output_dict = get_output_dict_from_google_events(google_events, max_event_results)
+        provider = GoogleCalendar(google_calendar_id, max_event_results, today_start_time, oneyearlater_iso)
+
+    calendar_events = provider.get_calendar_events()
+    output_dict = get_formatted_calendar_events(calendar_events)
 
     logging.info("main() - {}".format(output_dict))
 

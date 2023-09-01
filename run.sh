477a94a49154fbeddf5390f34ea7b736d3281edc
Christopher Rutsch <christopher.rutsch@student.uni-luebeck.de>
2023-09-02 00:20:26 +0200

    Add xkcd_get.py to fetch a xkcd-comic. Add a PRIVACY_mode into env.sh. Integrate the privacy mode into the run.sh

--- run.sh
+++ run.sh
@@ -7,36 +7,39 @@ function log {
     echo "---------------------------------------"
 }
 
-log "Add weather info"
-.venv/bin/python3 screen-weather-get.py
+if [ $PRIVACY_MODE = 1 ]; then
+    log "Display on epaper"
+    .venv/bin/python3 display.py xkcd-comic-strip.png
 
-log "Add Calendar info"
-.venv/bin/python3 screen-calendar-get.py
-
-if [ -f screen-custom-get.py ]; then
-    log "Add Custom data"
-    .venv/bin/python3 screen-custom-get.py
-elif [ ! -f screen-output-custom-temp.svg ]; then
-    # Create temporary empty svg since the main SVG needs it
-    echo "<svg />" > screen-output-custom-temp.svg
-fi
-
-
-log "Export to PNG"
-
-if [ $WAVESHARE_EPD75_VERSION = 1 ]; then
-    WAVESHARE_WIDTH=640
-    WAVESHARE_HEIGHT=384
 else
-    WAVESHARE_WIDTH=800
-    WAVESHARE_HEIGHT=480
+
+    log "Add weather info"
+    .venv/bin/python3 screen-weather-get.py
+
+    log "Add Calendar info"
+    .venv/bin/python3 screen-calendar-get.py
+
+    if [ -f screen-custom-get.py ]; then
+        log "Add Custom data"
+        .venv/bin/python3 screen-custom-get.py
+    elif [ ! -f screen-output-custom-temp.svg ]; then
+        # Create temporary empty svg since the main SVG needs it
+        echo "<svg />" > screen-output-custom-temp.svg
+    fi
+
+
+    log "Export to PNG"
+
+    if [ $WAVESHARE_EPD75_VERSION = 1 ]; then
+        WAVESHARE_WIDTH=640
+        WAVESHARE_HEIGHT=384
+    else
+        WAVESHARE_WIDTH=800
+        WAVESHARE_HEIGHT=480
+    fi
+
+    .venv/bin/cairosvg -o screen-output.png -f png --dpi 300 --output-width $WAVESHARE_WIDTH --output-height $WAVESHARE_HEIGHT screen-output-weather.svg
+
+
+    .venv/bin/python3 display.py screen-output.png
 fi
-
-
-## Hier if else für App integration einbringen
-
-
-.venv/bin/cairosvg -o screen-output.png -f png --dpi 300 --output-width $WAVESHARE_WIDTH --output-height $WAVESHARE_HEIGHT screen-output-weather.svg
-
-log "Display on epaper"
-.venv/bin/python3 display.py screen-output.png
--- xkcd_get.py
+++ xkcd_get.py
@@ -1,15 +1,58 @@
 import requests
 import json
+import wget
+import logging
+import os
+import numpy as np
+from PIL import Image
 
 
-
+    
 
 def xkcd_get_img():
+    logging.info("Downloading xkcd-json")
     response = requests.get("https://xkcd.com/info.0.json")
     result = response.json()
 
-    return result["img"]
+    logging.info("Downloading xkcd_img")
+    logging.info(result["img"])
+
+    path = 'path/to/your/waveshare/implementation'
+    filename = path + '/' + os.path.basename("xkcd-comic-strip.png")
+    if os.path.exists(filename):
+        os.remove(filename)
+    wget.download(result["img"], "xkcd-comic-strip.png")
+    
+    # Convert grayscale to pseudo-rgb since grayscale confuses the eink display
+
+    image = np.array(Image.open(filename).convert('RGB'))
+    Image.fromarray(image).save('xkcd-comic-strip.png')
+
+
+    """ If the downloaded image is too big to fit into the display downscale it
+    while taking the scales into account.
+    Afterwards resize the image to fit the screen. Sligh disortions can happen """
+    
+    im = Image.open(filename)
+    logging.debug("Png size: ",im.size)
+    if (im.size[0] > 800 and im.size[1] > 480):
+        logging.debug("Picture is bigger than eink-display")
+        print("After using thumbnail: ",im.size)
+        im.thumbnail((800,480))
+
+    if (im.size[0] < 800 and im.size[1] < 480):
+        im = im.resize((800,480))
+        logging.debug("Sizing onto eink-display size: ", im.size)
+    im.save(filename, "PNG")
+
+
+def main():
+    logging.info("Accessing API of xkcd")
+    xkcd_get_img()
 
 
 
 
+
+if __name__ == "__main__":
+    main()

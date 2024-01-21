4f6e3c363625f5e882741876afeefdb95e87af9e
mendhak <mendhak@users.noreply.github.com>
2024-01-21 10:44:41 +0000

    Programmatically generate the calendar and insert into template

--- screen-calendar-month.py
+++ screen-calendar-month.py
@@ -1,40 +1,64 @@
-import calendar
 import datetime
-import os.path
-import os
+import calendar
+import svgwrite
+from utility import update_svg, configure_locale, configure_logging
+import locale
+import babel
 import logging
-from utility import get_formatted_time, update_svg, configure_logging, get_formatted_date, configure_locale
+from collections import deque
 
-configure_locale()
+
 configure_logging()
+configure_locale()
 
-def get_formatted_calendar_month() -> dict:
-    formatted_calendar_month = {}
-    week_headers = calendar.weekheader(2).split()
-    for d in range(len(week_headers)):
-      formatted_calendar_month[f'WH{d}'] = week_headers[d]
-    today = datetime.date.today()
-    firstWeekDay, days = calendar.monthrange(today.year, today.month)
-    for d in range(37):
-        if d < firstWeekDay or d >= firstWeekDay + days:
-            formatted_calendar_month[f'D{d:0>{2}}'] = ""
-        else:
-            formatted_calendar_month[f'D{d:0>{2}}'] = f'{d - firstWeekDay + 1}'
-            if d - firstWeekDay + 1 == today.day:
-                formatted_calendar_month[f'C{d:0>{2}}'] = "important"
-    return formatted_calendar_month
+# Set the first weekday based on locale
+# Handle UnknownLocaleError
+try:
+    babel_locale = babel.Locale(locale.getlocale()[0])
+    logging.info(babel_locale)
+except babel.core.UnknownLocaleError:
+    logging.error("Could not get locale from environment. ")
 
 
-def main():
-    output_svg_filename = 'screen-output-weather.svg'
+calendar.setfirstweekday(babel_locale.first_week_day)
 
-    output_dict = get_formatted_calendar_month()
+# Get current year and month
+now = datetime.datetime.now()
+current_year, current_month, current_day = now.year, now.month, now.day
 
-    logging.info("main() - {}".format(output_dict))
+# Get the month's calendar
+cal = calendar.monthcalendar(current_year, current_month)
 
-    logging.info("Updating SVG")
-    update_svg(output_svg_filename, output_svg_filename, output_dict)
+# Create a new SVG drawing
+dwg = svgwrite.Drawing(profile='tiny')
+dwg['transform'] = 'translate(500, 240)'
+dwg['id'] = 'month-cal'
 
+# Define dimensions
+# cell_size = 40
+cell_width = 40
+cell_height = 30
+width = 7 * cell_width
+height = (len(cal) + 1) * cell_height
 
-if __name__ == "__main__":
-    main()
+# Have the day abbreviations respect the locale's first day of the week
+day_abbr = deque(list(calendar.day_abbr))
+day_abbr.rotate(-calendar.firstweekday())
+
+# Draw days of the week
+for i, day in enumerate(day_abbr):
+    dwg.add(dwg.text(day[:2], insert=(i*cell_width + 20, 20), fill='black'))
+
+# Draw days of the month
+for i, week in enumerate(cal):
+    for j, day in enumerate(week):
+        if day != 0:  # calendar.monthcalendar pads with 0s
+            text = dwg.text(day, insert=(j*cell_width + 20, (i+2)*cell_height - 10), fill='black')
+            if day == current_day:
+                text['class'] = 'important'
+            dwg.add(text)
+
+logging.info(dwg.tostring())
+update_svg('screen-output-weather.svg', 'screen-output-weather.svg', {'MONTH_CAL': dwg.tostring()})
+
+# Don't save the SVG, it will write a file to disk.
